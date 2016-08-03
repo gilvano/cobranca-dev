1d5109de2a12c0a37a0327f4852e938dca0114ff
gilpi <gilpi@DESKTOP-4PNTU7F>
2016-08-02 21:50:28 -0300

    adicionar o que já estava pronto

---
+++ src/main/java/com/gepe/cobranca/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.gepe.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gepe.cobranca.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long>{
+
+}
